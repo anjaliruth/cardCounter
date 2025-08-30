868b423194f12a404dcdb23ad2a4ed04c0aef52f
anjali <anjali.muthusamy@iceland.co.uk>
2025-08-30 12:37:57 +0100

    styling of playeraction buttons

--- src/IndividualPlayer.jsx
+++ src/IndividualPlayer.jsx
@@ -9,32 +9,40 @@ export default function IndividualPlayer({
   stay,
 }) {
   return (
-    <div className="individualCardStack">
-      <h1>Player {playerIndex + 1}</h1>
-      {hands[playerIndex]?.map((card, cardIndex) => (
-        <div
-          key={cardIndex}
-          className="positionHolder"
-          style={{
-            top: `${cardIndex * 15}px`,
-            left: `${cardIndex * 20}px`,
-          }}
-        >
-          <Card value={card.value} suit={card.suit} />
-        </div>
-      ))}
-      {initialCardsOut &&
-        playerIndex !== hands.length - 1 &&
-        playerIndex === position && (
-          <div className="playerActions">
-            <button className="cardActionButton" onClick={hitCards}>
-              🥊
-            </button>
-            <button className="cardActionButton" onClick={stay}>
-              🛑
-            </button>
+    <div className="stackAndAction">
+      <div className="individualCardStack">
+        <h1>Player {playerIndex + 1}</h1>
+        {hands[playerIndex]?.map((card, cardIndex) => (
+          <div
+            key={cardIndex}
+            className="positionHolder"
+            style={{
+              top: `${cardIndex * 15}px`,
+              left: `${cardIndex * 20}px`,
+            }}
+          >
+            <Card value={card.value} suit={card.suit} />
           </div>
-        )}
+        ))}
+      </div>
+      <div
+        className="playerActions"
+        style={{
+          visibility:
+            initialCardsOut &&
+            playerIndex !== hands.length - 1 &&
+            playerIndex === position
+              ? "visible"
+              : "hidden",
+        }}
+      >
+        <button className="cardActionButton" onClick={hitCards}>
+          🥊
+        </button>
+        <button className="cardActionButton" onClick={stay}>
+          🛑
+        </button>
+      </div>
     </div>
   );
 }
